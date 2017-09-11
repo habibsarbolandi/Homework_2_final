15df40488bc4938970f2f98a80ea31836962745a
habibsarbolandi <jsarbola@asu.edu>
2017-09-11 15:18:09 -0700

    uses simpler math operational

--- calculator_hw2/ViewController.swift
+++ calculator_hw2/ViewController.swift
@@ -15,31 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var mathDone: UILabel! // displays each number and operation as it is written
     
     var operationString: String = ""; // variable to store numerical operational
+    var expressionRead: String = ""; // empty string used for reading the math completed
+    
     
     // action triggered with number buttons pressed (watched a tutorial on youtube for the function)
     @IBAction func numberButtons(_ sender: UIButton) {
         mathDone.text = mathDone.text! + String(sender.tag); // writes the number
+        expressionRead = expressionRead + String(sender.tag);
+        
         }
     
+    // ction triggered by math or other numericals
     @IBAction func mathButtons(_ sender: UIButton) {
         if mathDone.text != "" && sender.tag != 10 && sender.tag != 15{ // this code prevents the user from putting a numerical operator in the blank label
             if sender.tag == 11 { //Divide
-                operationString = " / "; // assign division sign to string
+                operationString = " ÷ "; // assign division sign to string
+                expressionRead = expressionRead + "/";
                 mathDone.text = mathDone.text! + operationString;
             }
                 
             if sender.tag == 12 { //Multiple
-                operationString = " * "; // assign multiple sign to string
+                operationString = " x "; // assign multiple sign to string
+                expressionRead = expressionRead + "*";
                 mathDone.text = mathDone.text! + operationString;
             }
                 
             if sender.tag == 13 { //Substract
                 operationString = " - "; // assign subtraction sign to string
+                expressionRead = expressionRead + "-";
                 mathDone.text = mathDone.text! + operationString;
             }
     
             if sender.tag == 14 { //Addition
                 operationString = " + "; // assign addition sign to string
+                expressionRead = expressionRead + "+";
                 mathDone.text = mathDone.text! + operationString;
             }
         }
@@ -47,11 +56,13 @@ class ViewController: UIViewController {
         if sender.tag != 10 && sender.tag != 15 { // this allows the user to place parathesis
             if sender.tag == 16 {  // first parathesis
                 operationString = "(";
+                expressionRead = expressionRead + operationString;
                  mathDone.text = mathDone.text! + operationString;
             }
             
             else if sender.tag == 17 {  // second parathesis
                 operationString = ")";
+                expressionRead = expressionRead + operationString;
                 mathDone.text = mathDone.text! + operationString;
             }
             
@@ -61,13 +72,14 @@ class ViewController: UIViewController {
             mathDone.text = "";
             finalAnswer.text = "";
             operationString = "";
+            expressionRead = "";
             
         }
         
         if mathDone.text != "" {
             if sender.tag == 15 { // this begins calculating the number
             // This snippet of code was taken from stackoverflow and can be found here: https://stackoverflow.com/questions/24704028/nsexpression-calculator-in-swift
-                let expressionMath = NSExpression(format: mathDone.text!)
+                let expressionMath = NSExpression(format: expressionRead)
                 if let final = expressionMath.expressionValue(with: nil, context: nil) as? Double {
                 finalAnswer.text = String(final); // this displays the final answer of the math used
                 }else {
@@ -78,11 +90,13 @@ class ViewController: UIViewController {
         
         if sender.tag == 18 { // add decimal
             operationString = ".";
+            expressionRead = expressionRead + operationString;
             mathDone.text = mathDone.text! + operationString;
         }
         
         if sender.tag == 19 { // change the sign of the number
             operationString = "-";
+            expressionRead = expressionRead + operationString;
             mathDone.text = mathDone.text! + operationString;
         }
     }
