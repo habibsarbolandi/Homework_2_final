2da8f7a46300223bb5cda907b26376b871d39354
habibsarbolandi <jsarbola@asu.edu>
2017-09-11 13:51:12 -0700

    used stackoverflow example

--- calculator_hw2/ViewController.swift
+++ calculator_hw2/ViewController.swift
@@ -6,170 +6,88 @@
 //  Copyright © 2017 Habib Sarbolandi. All rights reserved.
 //
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
-    var numberScreen: Double = 0;
-    var numberPrevious: Double = 0;
-    var doingMath = false;
-    var operationString: String = "";
-    var screenNumber: String = "";
-    var operation = 0;
-    var negativeNumber1 = false;
-    var negativeNumber2 = false;
     
     
     // References for objects
-    @IBOutlet weak var finalAnswer: UILabel!
-    @IBOutlet weak var mathDone: UILabel!
+    @IBOutlet weak var finalAnswer: UILabel! // displays the final answer
+    @IBOutlet weak var mathDone: UILabel! // displays each number and operation as it is written
+    
+    var operationString: String = ""; // variable to store numerical operational
     
     // action triggered with number buttons pressed (watched a tutorial on youtube for the function)
     @IBAction func numberButtons(_ sender: UIButton) {
-        
-        if doingMath == true {
-            screenNumber = "";
-            screenNumber = screenNumber + String(sender.tag);
-            numberScreen = Double(screenNumber)!
-            
-            mathDone.text = mathDone.text! + String(sender.tag);
-            doingMath = false
+        mathDone.text = mathDone.text! + String(sender.tag); // writes the number push
         }
-            
-        else {
-            screenNumber = screenNumber + String(sender.tag);
-            numberScreen = Double(screenNumber)!
-            
-            mathDone.text = mathDone.text! + String(sender.tag);
-        }
-    }
     
-    
-    // this changes the shows the negative sign on the mathDone labeel
-    @IBAction func changeSign(_ sender: UIButton) {
-        mathDone.text = mathDone.text! + " -"
-        
-        // this function allows the user to input numbers after the negative sign
-        func numberButtons(_ sender: UIButton) {
-            
-            if doingMath == true {
-                screenNumber = "";
-                screenNumber = screenNumber + String(sender.tag);
-                numberScreen = Double(screenNumber)!
-                
-                mathDone.text = mathDone.text! + String(sender.tag);
-                doingMath = false
-            }
-                
-            else {
-                screenNumber = screenNumber + String(sender.tag);
-                numberScreen = Double(screenNumber)!
-                
-                mathDone.text = mathDone.text! + String(sender.tag);
-            }
-        }
-        
-        // shows the number is negative
-        if numberPrevious == 0 {
-            negativeNumber1 = true;
-        }
-        
-        else if numberPrevious != 0 {
-            negativeNumber2 = true;
-        }
-        
-    }
-    
-    
-    // this displays the operation command the user wishes to use
     @IBAction func mathButtons(_ sender: UIButton) {
-        if mathDone.text != "" && sender.tag != 10 && sender.tag != 15{
-            
-            numberPrevious = numberScreen;
-            
+        if mathDone.text != "" && sender.tag != 10 && sender.tag != 15{ // this code prevents the user from putting a numerical operator in the blank label
             if sender.tag == 11 { //Divide
-                operationString = " ÷ ";
+                operationString = " ÷ "; // assign division sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
                 
             else if sender.tag == 12 { //Multiple
-                operationString = " x ";
+                operationString = " x "; // assign multiple sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
                 
             else if sender.tag == 13 { //Substract
-                operationString = " - ";
+                operationString = " - "; // assign subtraction sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
-                
+    
             else if sender.tag == 14 { //Addition
-                operationString = " + ";
+                operationString = " + "; // assign addition sign to string
                 mathDone.text = mathDone.text! + operationString;
-                
-            }
-            
-            operation = sender.tag // asigns value pushed to operation
-            doingMath = true; // shows we are doing math
-        }
-            
-        // this is used to when the user pushes the equal sign
-        else if sender.tag == 15 {
-            
-            if operation == 11 { // Division
-                if negativeNumber1 == true {
-                    numberPrevious = -numberPrevious;
-                }
-                else if negativeNumber2 == true {
-                    numberScreen = -numberScreen;
-                }
-                
-                finalAnswer.text = String( numberPrevious / numberScreen);
-            }
-                
-            else if operation == 12 { // Multiplication
-                if negativeNumber1 == true {
-                    numberPrevious = -numberPrevious;
-                }
-                else if negativeNumber2 == true {
-                    numberScreen = -numberScreen;
-                }
-                
-                finalAnswer.text = String( numberPrevious * numberScreen);
-            }
-                
-            else if operation == 13 { // Subtraction
-                if negativeNumber1 == true {
-                    numberPrevious = -numberPrevious;
-                }
-                else if negativeNumber2 == true {
-                    numberScreen = -numberScreen;
-                }
-                
-                finalAnswer.text = String( numberPrevious - numberScreen);
-            }
-                
-            else if operation == 14 { // Addition
-                if negativeNumber1 == true {
-                    numberPrevious = -numberPrevious;
-                }
-                else if negativeNumber2 == true {
-                    numberScreen = -numberScreen;
-                }
-                
-                finalAnswer.text = String( numberPrevious + numberScreen);
             }
         }
+        
+        else if sender.tag != 10 && sender.tag != 15 { // this allows the user to place parathesis
+            if sender.tag == 16 {  // first parathesis
+                operationString = " (";
+                 mathDone.text = mathDone.text! + operationString;
+            }
             
-        else if sender.tag == 10 {
+            else if sender.tag == 17 {  // second parathesis
+                operationString = ")";
+                mathDone.text = mathDone.text! + operationString;
+            }
+            
+        }
+            
+        else if sender.tag == 10 { // this clears the operation string and both (mathDOne nad final) labels on the screeen
             mathDone.text = "";
             finalAnswer.text = "";
-            numberPrevious = 0;
-            numberScreen = 0;
-            operation = 0;
             operationString = "";
-            screenNumber = "";
             
         }
-}
+            
+        else if sender.tag == 15 { // this begins calculating the number
+            // This snippet of code was taken from stackoverflow and can be found here: https://stackoverflow.com/questions/24704028/nsexpression-calculator-in-swift
+            let expressionMath = NSExpression(format: mathDone.text!)
+            if let final = expressionMath.expressionValue(with: nil, context: nil) as? Double {
+                finalAnswer.text = String(final); // this displays the final answer of the math used
+            }else {
+                finalAnswer.text = "Numerical Error";
+            }
+            
+        }
+        
+        else if sender.tag == 18 { // add decimal
+            operationString = ".";
+            mathDone.text = mathDone.text! + operationString;
+        }
+        
+        else if sender.tag == 19 { // change the sign of the number
+            operationString = "-";
+            mathDone.text = mathDone.text! + operationString;
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -180,6 +98,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
 }
