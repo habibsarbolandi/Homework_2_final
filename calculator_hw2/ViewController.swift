6ec7b4b90b7d1d8c554834dfe287c8dcecd13c54
habibsarbolandi <jsarbola@asu.edu>
2017-09-11 14:48:25 -0700

    no error when equals is used with empty label

--- calculator_hw2/ViewController.swift
+++ calculator_hw2/ViewController.swift
@@ -10,8 +10,6 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    
-    
     // References for objects
     @IBOutlet weak var finalAnswer: UILabel! // displays the final answer
     @IBOutlet weak var mathDone: UILabel! // displays each number and operation as it is written
@@ -20,35 +18,35 @@ class ViewController: UIViewController {
     
     // action triggered with number buttons pressed (watched a tutorial on youtube for the function)
     @IBAction func numberButtons(_ sender: UIButton) {
-        mathDone.text = mathDone.text! + String(sender.tag); // writes the number push
+        mathDone.text = mathDone.text! + String(sender.tag); // writes the number
         }
     
     @IBAction func mathButtons(_ sender: UIButton) {
         if mathDone.text != "" && sender.tag != 10 && sender.tag != 15{ // this code prevents the user from putting a numerical operator in the blank label
             if sender.tag == 11 { //Divide
-                operationString = " ÷ "; // assign division sign to string
+                operationString = " / "; // assign division sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
                 
-            else if sender.tag == 12 { //Multiple
-                operationString = " x "; // assign multiple sign to string
+            if sender.tag == 12 { //Multiple
+                operationString = " * "; // assign multiple sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
                 
-            else if sender.tag == 13 { //Substract
+            if sender.tag == 13 { //Substract
                 operationString = " - "; // assign subtraction sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
     
-            else if sender.tag == 14 { //Addition
+            if sender.tag == 14 { //Addition
                 operationString = " + "; // assign addition sign to string
                 mathDone.text = mathDone.text! + operationString;
             }
         }
         
-        else if sender.tag != 10 && sender.tag != 15 { // this allows the user to place parathesis
+        if sender.tag != 10 && sender.tag != 15 { // this allows the user to place parathesis
             if sender.tag == 16 {  // first parathesis
-                operationString = " (";
+                operationString = "(";
                  mathDone.text = mathDone.text! + operationString;
             }
             
@@ -59,30 +57,31 @@ class ViewController: UIViewController {
             
         }
             
-        else if sender.tag == 10 { // this clears the operation string and both (mathDOne nad final) labels on the screeen
+        if sender.tag == 10 { // this clears the operation string and both (mathDOne nad final) labels on the screeen
             mathDone.text = "";
             finalAnswer.text = "";
             operationString = "";
             
         }
-            
-        else if sender.tag == 15 { // this begins calculating the number
+        
+        if mathDone.text != "" {
+            if sender.tag == 15 { // this begins calculating the number
             // This snippet of code was taken from stackoverflow and can be found here: https://stackoverflow.com/questions/24704028/nsexpression-calculator-in-swift
-            let expressionMath = NSExpression(format: mathDone.text!)
-            if let final = expressionMath.expressionValue(with: nil, context: nil) as? Double {
+                let expressionMath = NSExpression(format: mathDone.text!)
+                if let final = expressionMath.expressionValue(with: nil, context: nil) as? Double {
                 finalAnswer.text = String(final); // this displays the final answer of the math used
-            }else {
+                }else {
                 finalAnswer.text = "Numerical Error";
+                }
             }
-            
         }
         
-        else if sender.tag == 18 { // add decimal
+        if sender.tag == 18 { // add decimal
             operationString = ".";
             mathDone.text = mathDone.text! + operationString;
         }
         
-        else if sender.tag == 19 { // change the sign of the number
+        if sender.tag == 19 { // change the sign of the number
             operationString = "-";
             mathDone.text = mathDone.text! + operationString;
         }
